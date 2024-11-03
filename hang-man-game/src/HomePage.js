c7cbbc5d4cf1e3e2f49392418b6b6597dced948c
Flavia Bobeica <bobeicaflavia8@gmail.com>
2024-11-03 10:41:32 +0200

    commit2

---
+++ hang-man-game/src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <>
+        
+        
+       <p class="text-lg"> HomePage</p>
+        
+        
+        </>
+  )
+}
+
+export default HomePage
